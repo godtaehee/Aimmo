9a36b6c844719a1d540e17f44a8db6dfa82dd823
taehee <wonderfulhuman@naver.com>
2021-11-02 12:11:00 +0900

    Feat: Restoring Router
    We remove Many Router to change MongoDB from SQLite
    
    But, we finally success in connecting MongoDB
    
    So, We need to restore router that temporally is removed

--- src/app/routes/applicationRouter.ts
+++ src/app/routes/applicationRouter.ts
@@ -1,15 +1,19 @@
-import express, { Router } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
-import { authRouter } from './auth.router';
-import { postRouter } from './post.router';
-import { pageRouter } from './page.router';
+import express, { Router } from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
+import { authRouter } from "./auth.router";
+import { postRouter } from "./post.router";
+import { commentRouter } from "./comment.router";
+import { pageRouter } from "./page.router";
 
 const router: Router = express.Router();
-const swaggerSpec = YAML.load(path.join(__dirname, '../../../build/swagger.yaml'))
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-router.use('/auth', authRouter);
-router.use('/post', postRouter);
+const swaggerSpec = YAML.load(
+  path.join(__dirname, "../../../build/swagger.yaml")
+);
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+router.use("/auth", authRouter);
+router.use("/post", postRouter);
+router.use("/comment", commentRouter);
 // router.use('/page', pageRouter);
 export const applicationRouter: Router = router;
