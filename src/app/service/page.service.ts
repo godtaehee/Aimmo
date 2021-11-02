17a6cebdf7875e776c1264e744d13642c9224dca
taehee <wonderfulhuman@naver.com>
2021-11-03 02:44:54 +0900

    Feat: Post Pagination for MongoDB(NoSQL)
    RDMBS can imporve pagination speed with Covering Index
    
    But I implement Pagination with no offset
    
    After task, I will search about covering Index in NoSQL
    
    If Client hit search Button, We return Post List which have limit length.
    
    After that, Server Request Last Post Id which is in above Post List
    
    With this LastId, We can improve Pagination Speed
    
    But We lost random-access Pagination
    
    MongoDB ObjectID can compare each other
    
    If you interested in above Issue, See https://darrengwon.tistory.com/826

--- src/app/service/page.service.ts
+++ src/app/service/page.service.ts
@@ -1,28 +1,31 @@
-// import { getConnection, QueryRunner, Repository } from "typeorm";
-// import { Post } from "../entity/post";
-//
-// export class PageService {
-//     private postRepository: Repository<Post>;
-//
-//     constructor() {
-//         this.postRepository = getConnection().getRepository(Post);
-//     }
-//
-//     async getPostList(pageInfo) {
-//         const postList = await
-//             this.postRepository
-//                 .createQueryBuilder('post')
-//                 .select(['post', 'user.email'])
-//                 .innerJoin('post.user', 'user')
-//                 .orderBy('post.id', 'DESC')
-//                 .limit(pageInfo.limit)
-//                 .offset(pageInfo.offset)
-//                 .disableEscaping()
-//                 .getMany()
-//         const postCount = await
-//             this.postRepository
-//                 .count();
-//         return { postList, postCount };
-//     }
-//
-// }
+import { getConnection, QueryRunner, Repository } from "typeorm";
+import { ObjectID } from "mongodb";
+import { Post } from "../entity/post";
+import { User } from "../entity/user";
+
+export class PageService {
+  private postRepository: Repository<Post>;
+  private queryRunner: QueryRunner;
+  private lastId: string;
+  constructor() {
+    this.queryRunner = getConnection().createQueryRunner();
+    this.postRepository = this.queryRunner.manager.getRepository(Post);
+  }
+
+  async getPostList(pageInfo) {
+    let postList = await this.postRepository.find({
+      take: pageInfo.limit,
+    });
+    if (pageInfo.offset !== "0") {
+      postList = await this.postRepository.find({
+        take: pageInfo.limit,
+        where: {
+          _id: { $gt: new ObjectID(pageInfo.offset) },
+        },
+      });
+    }
+    this.lastId = postList[postList.length - 1].id.toString();
+
+    return { postList };
+  }
+}
