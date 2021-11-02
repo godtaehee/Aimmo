3fec636eaf0df201ad7b4d4cacf9ac6c32a61885
taehee <wonderfulhuman@naver.com>
2021-11-02 12:06:55 +0900

    Feat: Required field in post entity for various posting API
    I set initial value using @BeforeInsert Decorator
    
    We need category field to implement Post's Category
    
    We need count field and views to implement Block Duplicate views
    
    We need comments field to implement comment and nested-comment

--- src/app/entity/post.ts
+++ src/app/entity/post.ts
@@ -1,11 +1,39 @@
-import {Base} from "./base.entity";
-import {Column, Entity} from "typeorm";
+import { Base } from "./base.entity";
+import { BeforeInsert, Column, Entity } from "typeorm";
+import { Comment } from "./comment";
+
+const BOARD_CATEGORY = {
+  WEB: "WEB",
+  APP: "APP",
+  GAME: "GAME",
+  ALGORITHM: "ALGORITHM",
+  INFRA: "INFRA",
+  DATABASE: "DATABASE",
+};
 
 @Entity()
 export class Post extends Base {
+  @BeforeInsert()
+  initValue() {
+    this.count = 0;
+    this.views = [];
+    this.comments = [];
+  }
   @Column()
   title: string;
 
   @Column()
   text: string;
+
+  @Column()
+  category: typeof BOARD_CATEGORY[keyof typeof BOARD_CATEGORY];
+
+  @Column()
+  count: number;
+
+  @Column()
+  views: string[];
+
+  @Column()
+  comments: Comment[];
 }
